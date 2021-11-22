672b31741c8d63a1b4385708ac66bd84d990f4c9
asifhjahid <dhrubo8211diu@gmail.com>
2021-11-22 21:15:37 +0600

    starting api

---
+++ README.md
@@ -0,0 +1,1 @@
+# node-uptime-monitoring-application
---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * Title: Uptime Monitoring Application
+ * Description: A RESTFul API to monitor up to down time of user defined links
+ * Autho: Asif Hasan Jahid
+ * Date: 22/11/2021
+ */
+
+// dependencise
+const http = require('http');
