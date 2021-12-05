2f8e8e0beee96993e42364b2889702ebbb15f771
asifhjahid <dhrubo8211diu@gmail.com>
2021-12-05 20:14:51 +0600

    CRUD

--- index.js
+++ index.js
@@ -8,25 +8,27 @@
 // dependencise
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments');
+const data = require('./lib/data');
 
 // app object - module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-    port: 3000,
-};
+// testin file system
+data.delete('test', 'newFile', (err) => {
+    console.log(err);
+});
 
 // create server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`listening to port ${app.config.port}`);
+    server.listen(environment.port, () => {
+        console.log(`listening to port ${environment.port}`);
     });
 };
 
 // handle Request Response
 app.handleReqRes = handleReqRes;
 
-// start the server  
+// start the server
 app.createServer();
